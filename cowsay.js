4ada37aba4074015a77313a34f5539234327cbaa
lsj <qazplm807733@gmail.com>
2022-09-14 17:20:59 +0900

    init

---
+++ cowsay.js
@@ -0,0 +1,13 @@
+const cowsay = require("cowsay");
+
+let moment = require("moment");
+
+// console.log(
+//   cowsay.say({
+//     text: "abc",
+//     e: "OO",
+//     T: "U ",
+//   })
+// );
+
+console.log(moment);
---
+++ lodash.js
@@ -0,0 +1,7 @@
+var _ = require("lodash");
+
+const arr = [5, 4, 3, 2, 1];
+
+const revArr = _.reverse(arr);
+
+console.log(revArr);
